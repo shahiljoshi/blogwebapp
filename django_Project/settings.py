110cae06a57085882e561c0a81c510c7673db00c
shahiljoshi <shahil.inexture@gmail.com>
2021-05-11 15:37:34 +0530

    feat:settings

--- django_Project/settings.py
+++ django_Project/settings.py
@@ -67,12 +67,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'django_Project.urls'
-
+# 'DIRS': [BASE_DIR / 'templates']
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
